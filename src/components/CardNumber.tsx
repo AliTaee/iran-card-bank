25e319ad34d5f9d376b95cb3b202c11dcb73e920
AliTaee <ali.taee@zoodfood.com>
2021-02-10 14:18:58 +0330

    Add error handeling for less 16 digit and write test

--- src/components/CardNumber.tsx
+++ src/components/CardNumber.tsx
@@ -9,12 +9,14 @@ const CardNumber: React.FC<CardNumberProps> = (props: CardNumberProps) => {
 
   return (
     <>
-      {cardNumber
-        .toString()
-        .match(/.{1,4}/g)
-        ?.map((fourNumber, index) => (
-          <span key={index}>{fourNumber}</span>
-        ))}
+      {cardNumber.toString().length < 16 ? (
+        <p className="icb-card-bank__error">The number is less than 16 digit</p>
+      ) : (
+        cardNumber
+          .toString()
+          .match(/.{1,4}/g)
+          ?.map((fourNumber, index) => <span key={index}>{fourNumber}</span>)
+      )}
     </>
   )
 }
--- src/test/CardNumber.test.tsx
+++ src/test/CardNumber.test.tsx
@@ -3,28 +3,36 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import CardNumber from '../components/CardNumber'
 
-describe('Renders card number correctly', () => {
-  beforeEach(() => {
-    render(<CardNumber cardNumber={5555_1111_2222_3333} />)
+describe('Card number tests', () => {
+  describe('Renders card number correctly', () => {
+    beforeEach(() => {
+      render(<CardNumber cardNumber={5555_1111_2222_3333} />)
+    })
+
+    it('Should have 5555', () => {
+      const firstCardNumber = screen.getByText(/5555/i)
+      expect(firstCardNumber).toBeInTheDocument()
+    })
+
+    it('Should have 1111', () => {
+      const secondCardNumber = screen.getByText(/1111/i)
+      expect(secondCardNumber).toBeInTheDocument()
+    })
+
+    it('Should have 2222', () => {
+      const thirdCardNumber = screen.getByText(/2222/i)
+      expect(thirdCardNumber).toBeInTheDocument()
+    })
+
+    it('Should have 3333', () => {
+      const fourthCardNumber = screen.getByText(/3333/i)
+      expect(fourthCardNumber).toBeInTheDocument()
+    })
   })
 
-  it('Should have 5555', () => {
-    const firstCardNumber = screen.getByText(/5555/i)
-    expect(firstCardNumber).toBeInTheDocument()
-  })
-
-  it('Should have 1111', () => {
-    const secondCardNumber = screen.getByText(/1111/i)
-    expect(secondCardNumber).toBeInTheDocument()
-  })
-
-  it('Should have 2222', () => {
-    const thirdCardNumber = screen.getByText(/2222/i)
-    expect(thirdCardNumber).toBeInTheDocument()
-  })
-
-  it('Should have 3333', () => {
-    const fourthCardNumber = screen.getByText(/3333/i)
-    expect(fourthCardNumber).toBeInTheDocument()
+  it('Should Render error with provide less than 16 digits', () => {
+    render(<CardNumber cardNumber={0} />)
+    const errorText = screen.getByText(/The number is less than 16 digit/i)
+    expect(errorText).toBeInTheDocument()
   })
 })
