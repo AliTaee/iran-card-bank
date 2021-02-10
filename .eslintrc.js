3552ee61a72428c6a491d24cb86c1a7cf21386a2
AliTaee <ali.taee@zoodfood.com>
2021-02-10 09:48:48 +0330

    Install scss, prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  parser: 'babel-eslint',
+  rules: {
+    strict: 0,
+    'linebreak-style': 0,
+    'prettier/prettier': 'error',
+    'testing-library/await-async-query': 'error',
+    'testing-library/no-await-sync-query': 'error',
+    'testing-library/no-debug': 'warn',
+  },
+  settings: {
+    react: {
+      version: '17.0.1',
+    },
+  },
+  plugins: ['prettier', 'testing-library', 'jest-dom'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+    'plugin:prettier/recommended',
+    'plugin:testing-library/react',
+    'plugin:testing-library/recommended',
+    'plugin:jest-dom/recommended',
+  ],
+}
