26f8927ee8597691ae93da35ca17336ccae400fe
AliTaee <ali.taee@zoodfood.com>
2021-02-10 17:05:18 +0330

    Add unit test for border radius prop

--- src/test/IranCardBank.test.tsx
+++ src/test/IranCardBank.test.tsx
@@ -3,13 +3,23 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import IranCardBank from '../components/IranCardBank'
 
+const TEST_ID_ELEMENT = 'icb-card-bank'
+const MOCK_CARD_NUMBER = 5555_1111_2222_3333
+
 describe('Iran card number test', () => {
   describe('Test props related to inline styles', () => {
     it('Should have custom background image', () => {
-      render(<IranCardBank cardNumber={5555_1111_2222_3333} background="#212121" />)
+      render(<IranCardBank cardNumber={MOCK_CARD_NUMBER} background="#212121" />)
 
-      const rootElement = screen.getByTestId('icb-card-bank')
+      const rootElement = screen.getByTestId(TEST_ID_ELEMENT)
       expect(rootElement).toHaveStyle('background: #212121')
+    })
+
+    it('Should have custom border radius', () => {
+      render(<IranCardBank cardNumber={MOCK_CARD_NUMBER} borderRadius="15px" />)
+
+      const rootElement = screen.getByTestId(TEST_ID_ELEMENT)
+      expect(rootElement).toHaveStyle('border-radius: 15px')
     })
   })
 })
